bf4ad650312c7282806b49c596ff6b5b76014943
Gregory Graham <gregory@gregs.co.nz>
2020-09-24 15:13:49 +1200

    Removed unused imports

--- src/main/java/nz/co/gregs/amhan/database/ClusterMonitorComponent.java
+++ src/main/java/nz/co/gregs/amhan/database/ClusterMonitorComponent.java
@@ -7,7 +7,6 @@ package nz.co.gregs.amhan.database;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
